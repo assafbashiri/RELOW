a864a2de22de6054f133be45573370f848f4126d
assafbashiri <71321725+assafbashiri@users.noreply.github.com>
2021-08-02 10:54:50 +0300

    Revert "Bussines layer imp - cont"
    This reverts commit 7bbd3a38302327c75476e0c1f09bf3f55b4887a0.

--- server/bussines/Controllers/UserController.py
+++ server/bussines/Controllers/UserController.py
@@ -1,58 +1,59 @@
-from BussinesLayer.Utils import CheckValidity
-from BussinesLayer.Objects.User import User
+from bussines.Utils import CheckValidity
+from bussines.Object.User import User
 
 
 class UserController():
     def __init__(self):
-        self.users_list = None # dictionary userId to user
+        self.users_list = None #dictionary userId to user
         self.user_id = 0
 
-    def add_user(self, first_name, last_name, user_name, user_id, email, password):
+    def addUser(self, first_name, last_name, user_name, user_id, email, password):
         user_to_add = User(self.user_id, first_name, last_name, user_name, email, password)
         user_to_add.active = True
+        self.user_id+=1
         self.users_list.add(user_id, user_to_add)
-        self.user_id += 1
-        # add to DB
-
-    def remove_user(self, user_id):
-        if self.users_list.get(user_id) is None:
+        #add to DB
+    def removeUser(self, user_id):
+        if  self.users_list.get(user_id) is None :
             raise Exception ("User does not exist")
 
         self.users_list.get(user_id).active = False #check
         self.users_list.get(user_id).key = None
-        # remove in DB
+        #remove in DB
 
-    def update_first_name(self, user_id, new_first_name):
+    def updateFirstName(self,user_id,new_first_name):
         user_to_update = self.getUserById(self,user_id)
         user_to_update.set_first_name(new_first_name)
 
-    def update_last_name(self,user_id, new_last_name):
+    def updateLastName(self,user_id, new_last_name):
         user_to_update = self.getUserById(self, user_id)
         user_to_update.set_last_name(new_last_name)
 
-    def update_user_name(self, user_id, new_user_name):
+
+    def updateUserName(self,user_id, new_user_name):
         user_to_update = self.getUserById(self, user_id)
         user_to_update.set_user_name(new_user_name)
 
     def get_user_by_id(self, user_id):
-        if user_id not in self.users_list.keys:
-            raise Exception("User Does Not Exist")
+        if not id  in self.users_list.keys:
+            raise Exception ("user does not exist")
         return self.users_list.get(user_id)
 
-    def register(self, first_name, last_name, user_name, email, password):
-        self.add_user(first_name, last_name, user_name, email, password)
+    def register(self, first_name, last_name,user_name, email, password):
+        self.addUser(first_name,last_name,user_name, email, password)
         # in add user change field "active" in user to add
-        # in "add_user" add the new user to DB
 
-    def log_in(self, user_name, password):
+
+
+
+    def log_in(self,user_name, password):
         if not self.exist_user_name(self, user_name):
             raise Exception("User Name Not Exist")
-        # user name exist in the dictionary
         password_of_user = self.get_password_by_user_name(self, user_name)
-        if password_of_user != password:
+        if not password_of_user != password:
             raise Exception("Illegal Password")
-        user_to_log_in = self.get_user_by_user_name(self, user_name)
-        user_to_log_in.log_in(self) # check this line
+        user_to_log_in = self.get_user_by_user_name (self, user_name)
+        user_to_log_in.log_in(self, user_name, password)
 
     def exist_user_name(self, user_name):
         user_ids = self.users_list.keys
@@ -68,17 +69,17 @@ class UserController():
                 return self.users_list.get(curr_user_id).password
         return None
 
-    def get_user_by_user_name(self, user_name):
+    def get_password_by_user_name(self, user_name):
         user_ids = self.users_list.keys
         for curr_user_id in user_ids:
             if user_name == self.users_list.get(curr_user_id).user_name:
                 return self.users_list.get(curr_user_id)
         return None
 
-    def log_out(self, user_id):
-        if user_id not in self.users_list.keys:
-            raise Exception("UserDoes Not Exist")
-        self.users_list.get(user_id).log_out(self)
+    def log_out (self, user_id):
+        if self.users_list.get(user_id) is None:
+            raise Exception("User Name Not Exist")
+        self.users_list.get(user_id).log_out(self, user_id)
 
 
 
---
+++ server/bussines/Object/SubCategory.py
@@ -0,0 +1,5 @@
+class SubCategory:
+    def __init__(self, name, nextId):
+        self.name=name
+        self.offer_list = None
+        self.id = nextId
