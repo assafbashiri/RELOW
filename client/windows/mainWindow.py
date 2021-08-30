84e7618bada8652742bccde0670b05718ad4f6a0
assafbashiri <71321725+assafbashiri@users.noreply.github.com>
2021-08-30 17:20:57 +0300

    update client

--- client/windows/mainWindow.py
+++ client/windows/mainWindow.py
@@ -2,20 +2,25 @@ from datetime import datetime
 
 from kivy.app import App
 from kivy.core.image import Image
+from kivy.event import EventDispatcher
 from kivy.lang import Builder
-from kivy.properties import StringProperty, ObjectProperty
+from kivy.properties import StringProperty, ObjectProperty, ListProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.carousel import Carousel
 from kivy.uix.image import AsyncImage
 from kivy.uix.label import Label
 from kivy.uix.recycleview import RecycleView
 from kivy.uix.popup import Popup
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.app import MDApp
 from kivy.uix.button import Button
 from kivymd.uix.label import MDLabel
 # from kivy.config import Config
 # Config.set('kivy', 'exit_on_escape', '0')
+from kivymd.uix.progressbar import MDProgressBar
+from kivymd.uix.slider import MDSlider
+
 from windows.accountWindow import ACCOUNTScreen
 from windows.connectWindow import CONNECTScreen
 from windows.searchWindow import SEARCHScreen
@@ -48,18 +53,41 @@ class Manager(ScreenManager):
 class MessageBox(Popup):
     def __init__(self, **kwargs):
         super(MessageBox, self).__init__(**kwargs)
+        self.title = "bob"
         self.box = BoxLayout(orientation= 'vertical')
-        self.carousel = Carousel()
+        self.carousel = Carousel(size_hint_y= 6)
         for photo in kwargs:
             image = Image(str(photo))
             self.carousel.add_widget(image)
+        image = AsyncImage(source="windows/images/a.png")
+        self.carousel.add_widget(image)
         self.box.add_widget(self.carousel)
-        self.name = Label(text= "name")
+        self.slider = MDSlider()
+        self.slider.min = 0
+        self.slider.max = 150
+        self.slider.value = 15
+        self.progress = MDProgressBar()
+        self.progress.value = self.slider.value
+        self.box.add_widget(self.progress)
+        self.name = Label(text = "name")
         self.box.add_widget(self.name)
-        self.company = "company"
-        self.description = "description"
-        self.product_size = "size"
-        self.color = "color"
+        self.company = Label(text = "company")
+        self.box.add_widget(self.company)
+        self.description = Label(text = "description")
+        self.box.add_widget(self.description)
+        self.product_size = Label(text = "size")
+        self.box.add_widget(self.product_size)
+        self.color = Label(text = "color")
+        self.box.add_widget(self.color)
+        self.join = Button(text= "JOIN")
+        self.box.add_widget(self.join)
+        self.back = Button(text="BACK")
+        self.back.bind(on_press = lambda x:self.dismiss())
+        self.box.add_widget(self.back)
+        self.add_widget(self.box)
+
+    def out(self):
+        self.dismiss()
 
 
 
@@ -78,8 +106,16 @@ class Offers_Screen_main(RecycleView):
     def insert_offers(self, **kwargs):
         # get the offer liat from the user
         # loop all the offer and add them to the recycle
+        lis =[]
+        im1 = AsyncImage(source = "windows/images/a.png")
+        lis.append(im1)
+        # for on offers lis
+        # create oofer
+        # add offer to lis
+        # set data to be lis
+        self.data = [ {'text1':"bolo", 'photo_lis':lis}, {'text1':'bolo2', 'photo_lis':lis} ]
+        # self.data[0]['more_details'].text = "bolo"
 
-        self.data = [{} for x in range(2)]
 
 
 class Menu_box(BoxLayout):
@@ -89,8 +125,6 @@ class Menu_box(BoxLayout):
         self.sub_cat = Sub_Category_box()
     def exit(self):
         App.get_running_app().controller.exit()
-    #     write to json
-        App.get_running_app().on_request_close()
     def change_to_cat(self):
         print(self.parent)
         self.side = self.ids.side_box
@@ -119,28 +153,21 @@ class Menu_box(BoxLayout):
         print('test press: ', message)
 
 
-class RecycleViewRow(BoxLayout):
-    text1 = StringProperty(name = "dd")
-    def __init__(self,**kwargs):
-        super(RecycleViewRow, self).__init__(**kwargs)
-        self.car = Carousel(direction='left', size_hint_y= 2)
-        # for photo in kwargs['photos']:
-        for photo in kwargs:
-            self.car.add_widget(photo)
-        self.add_widget(self.car)
-        # self.add_widget(Label(text=kwargs['name']))
-        self.add_widget(MDLabel(text="name",size_hint_y= .5))
-        # self.add_widget(Label(text=kwargs['company']))
-        self.add_widget(MDLabel(text="company",size_hint_y= .5))
-        # self.add_widget(Label(text=kwargs['description']))
-        self.add_widget(MDLabel(text="description",size_hint_y= .5))
-        # self.add_widget(Label(text=kwargs['size']))
-        self.add_widget(MDLabel(text="size",size_hint_y= .5))
-        # self.add_widget(Label(text=kwargs['photo']))
-        self.more_details = Button(text="more details",size_hint_y=.5 ,on_press= lambda a:self.www())
-        self.more = Button(text="more", size_hint_y=.5 ,on_press= lambda a:self.insert())
-        self.add_widget(self.more_details)
-        self.add_widget(self.more)
+
+class RecycleViewRow(RecycleDataViewBehavior,BoxLayout):
+    text1 = StringProperty()
+    photo_lis = ListProperty()
+    # def __init__(self,**kwargs):
+    #     super(RecycleViewRow, self).__init__(**kwargs)
+    #     self.car = Carousel(direction='left', size_hint_y= 2)
+    #     # for photo in kwargs['photos']:
+    #     for photo in kwargs:
+    #         self.car.add_widget(photo)
+    #     self.add_widget(self.car)
+    #     self.more_details = Button(text="more details",size_hint_y=.5 ,on_press= lambda a:self.www())
+    #     self.more = Button(text="more", size_hint_y=.5 ,on_press= lambda a:self.insert())
+    #     self.add_widget(self.more_details)
+    #     self.add_widget(self.more)
     def insert(self, **kwargs):
         im = AsyncImage(source="windows/images/e.png")
         im1 = AsyncImage(source="windows/images/c.png")
@@ -150,16 +177,29 @@ class RecycleViewRow(BoxLayout):
         m = MessageBox().open()
 
 
+class Carousel2(Carousel):
+    sourc = StringProperty()
+    def __init__(self,**kwargs):
+        super(Carousel2, self).__init__(**kwargs)
+
+
+
+    def insert(self,  text):
+        a = 5
+        print(text)
+        # for photo in lis:
+        #     self.add_widget(photo)
 
 class Carousel1(Carousel):
     sourc = StringProperty()
     def __init__(self,**kwargs):
         super(Carousel1, self).__init__(**kwargs)
+
     def insert(self, **kwargs):
+        print('name')
         im = AsyncImage(source="windows/images/e.png")
         im1 = AsyncImage(source="windows/images/c.png")
         self.add_widget(im)
-
 
 
 class TestApp(MDApp):
