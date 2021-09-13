6d65fdcbf5487e9f88ec0e7caea06b80bc3c3067
tomnisim <tomnisi@post.bgu.ac.il>
2021-09-13 19:45:17 +0300

    checkvalidity minor changes

--- client/Utils/CheckValidity.py
+++ client/Utils/CheckValidity.py
@@ -1,50 +1,86 @@
 import re
 import datetime
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+from kivymd.toast import toast
 
 class CheckValidity:
     def checkValidityName(self,name):
+        if ' ' in name:
+            res = "Name Shouldnt contain any spaces"
+            toast(res)
+            return False
         if len(name)<=1:
-            return "name is too short", False
+            res =  "name is too short"
+            toast(res)
+            return False
         if name.replace(" ", "").isalpha():
-            return "Good Name" ,True
+            res =  "Good Name"
+            toast(res)
+            return True
         else:
-            return "Bad Name - name should contain only letters",False
+            res =  "Bad Name - name should contain only letters"
+            toast(res)
+            return False
 
     # user name length is 8-20 letters
     def checkValidityUserName(self, user_name):
         if len(user_name)<4:
-            return "User Name Must contain at least 4 letters", False
+            res = "User Name Must contain at least 4 letters"
+            toast(res)
+            return False
         if len(user_name)>20:
-            return "User Name Must contain maximum of 20 letters", False
+            res = "User Name Must contain maximum of 20 letters"
+            toast(res)
+            return False
         if not user_name[0].isalpha():
-            return "User Name Should Start With letter"
+            res = "User Name Should Start With letter"
+            toast(res)
+            return False
         if ' ' in user_name:
-            return "User Name Shouldnt contain any spaces"
-        #add an regex to validate that the user name contain only letters and numbers
+            res = "User Name Shouldnt contain any spaces"
+            toast(res)
+            return False
         else:
-            return "Good User Name", True
+            res = "Good User Name"
+            toast(res)
+            return  True
 
     def checkValidityEmail(self, email):
         # have to check if email exist in the world
         if not re.match(regex, email):
-            return "Bad Email",False
-        return "Good Email",True
+            res = "Bad Email"
+            toast(res)
+            return False
+        res= "Good Email"
+        toast(res)
+        return True
 
     #password length 8-20
     def checkValidityPassword(self, password):
             if len(password) < 8:
-                return "Make sure your password is at lest 8 letters", False
+                res= "Make sure your password is at lest 8 letters"
+                toast(res)
+                return False
             elif len(password) > 20:
-                return "Make sure your password is less then 20 letters", False
+                res= "Make sure your password is less then 20 letters"
+                toast(res)
+                return False
             elif not any(char.isdigit() for char in password):
-                return "Make sure your password has a number in it", False
+                res = "Make sure your password has a number in it"
+                toast(res)
+                return False
             elif not any(char.isupper() for char in password):
-                return "Make sure your password has a lower letter in it", False
+                res= "Make sure your password has a lower letter in it"
+                toast(res)
+                return False
             elif not any(char.islower() for char in password):
-                return "Make sure your password has a capital letter in it", False
+                res= "Make sure your password has a capital letter in it"
+                toast(res)
+                return False
             else:
-                return "Your password seems fine", True
+                res ="Your password seems fine"
+                toast(res)
+                return True
 
     #check if this function is important due to date picker
     def checkValidityDateOfBirth(self, date):
